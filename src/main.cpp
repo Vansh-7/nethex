70f6b0689af4c131efa0b259b5af4befa1eb50c1
Vansh Gupta <vanshgupta1490@gmail.com>
2026-07-06 20:41:37 +0530

    feat(engine): integrate packet slicer into core ingestion loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "platform.h"
 #include "pcap_reader.h"
+#include "packet_parser.h"
 #include <iostream>
 
 int main() {
@@ -17,6 +18,20 @@ int main() {
         // The loop just asks for the next packet.
         while (reader->read_next_packet(packet_data, packet_length)) {
             packet_count++;
+            
+            // let's print first 3 packets
+            if (packet_count <= 3) {
+                std::cout << "\n[+] Packet #" << packet_count << " (Size: " << packet_length << " bytes)" << std::endl;
+                
+                uint16_t next_protocol = 0;
+                if (NetHex::PacketParser::parse_ethernet(packet_data, packet_length, next_protocol)) {
+                    // EtherType 0x0800 means the encapsulated data is IPv4
+                    if (next_protocol == 0x0800) {
+                        // Offset the pointer by the size of the Ethernet header to get to the IP header
+                        NetHex::PacketParser::parse_ipv4(packet_data, packet_length, sizeof(NetHex::EthernetHeader));
+                    }
+                }
+            }
         }
 
         std::cout << "[NetHex] Successfully ingested " << packet_count << " packets." << std::endl;
